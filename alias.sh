595f1ff1912b4a468ef44bc57c9f2b1aa1adf5f2
loheagn <linan_123@buaa.edu.cn>
2019-05-18 22:34:36 +0800

    add alias homes

--- alias.sh
+++ alias.sh
@@ -5,3 +5,9 @@ alias cin='code-insiders'
 alias aliyun="ssh 'loheagn@39.96.166.183'"
 
 alias vultr="ssh 'loheagn@2001:19f0:6001:444c:5400:02ff:fe02:730e'"
+
+alias homecode="cd /mnt/c/Users/LiNan/OneDrive/SourceCode/"
+
+alias home="cd /mnt/c/Users/LiNan/"
+
+alias homedoc="cd /mnt/c/Users/LiNan/OneDrive/文档/"
