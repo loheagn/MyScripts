d4ea4dc64a02f51a6d4abfad10324574b7e20d91
loheagn <loheagn@loheagndeMacBook-Pro.local>
2019-05-22 23:49:51 +0800

    nothing

--- initShell.sh
+++ initShell.sh
@@ -12,6 +12,6 @@ alias home="cd /mnt/c/Users/LiNan/"
 
 alias homedoc="cd /mnt/c/Users/LiNan/OneDrive/文档/"
 
-alias code="cd /Volumes/Data/Code"
+alias code="cd /Users/loheagn/OneDrive/Code"
 
 export PATH=/Users/loheagn/MyScripts:$PATH
