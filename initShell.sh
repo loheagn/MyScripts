5cd8bdd90874f1d073428780126fb0400ae40935
loheagn <loheagn@linandeMacBook-Pro.local>
2019-07-12 15:45:19 +0800

    修改vultr服务器地址

--- initShell.sh
+++ initShell.sh
@@ -12,9 +12,9 @@ alias aliyun="ssh 'loheagn@39.96.166.183'"
 
 alias aliyun-root="ssh 'root@39.96.166.183'"
 
-alias vultr="ssh 'loheagn@2001:19f0:6801:12d1:5400:02ff:fe10:54e3'"
+alias vultr="ssh 'loheagn@95.179.208.47'"
 
-alias vultr-root="ssh 'root@2001:19f0:6801:12d1:5400:02ff:fe10:54e3'"
+alias vultr-root="ssh 'root@95.179.208.47'"
 
 alias ccode="cd ~/Code"
 
